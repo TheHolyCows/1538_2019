e337bc6cfa017782cfc5c6cfe77359875b99a840
The Holy Cows <theholycows@172-15-38-138.lightspeed.frokca.sbcglobal.net>
2019-04-03 00:26:21 -0700

    Franny - added cargo hatch autos-

--- src/Autonomous/AutoModes.cpp
+++ src/Autonomous/AutoModes.cpp
@@ -37,6 +37,50 @@ AutoModes::AutoModes()
 	m_Modes["Two Hatch Rocket - Right"].push_back(RobotCommand(CMD_INTAKE_EXHAUST, 0, 45, 0, CONSTANT("HATCH_ELV_LVL1"), false, 0.5));
 	m_Modes["Two Hatch Rocket - Right"].push_back(RobotCommand(CMD_HOLD_DISTANCE, -24, 133, 0.35, CONSTANT("HATCH_ELV_LVL1"), false, 2));
 	m_Iterator = m_Modes.begin();
+
+   
+	m_Modes["Two Hatch Cargo Ship - Right"];
+    m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_RIGHT_ENCODER, 0, 0, 0, 5, true, 0.01)); 
+	//m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_PRESTART, 0, 0, 0, 0, true, 2)); 
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_ARM_DOWN, 0, 0, 0, 0, true, 0.25));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, 45, 0, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 1.5));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_VISION_ALIGN, 0, 0, -0.2, CONSTANT("HATCH_ELV_LVL1"), false, 7));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_INTAKE_EXHAUST, 0, 0, 0, CONSTANT("HATCH_ELV_LVL1"), false, 0.34));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, -18, 0, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 1.5));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_TURN, -18, 100, 0.7, CONSTANT("HATCH_ELV_LVL1"), true, 0.7));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, 69, 100, 0.64, CONSTANT("HATCH_ELV_LVL1"), false, 3));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, 77, 155, 0.4, CONSTANT("HATCH_ELV_LVL1"), false, 1.5));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_VISION_HATCH_INTAKE, 0, 180, -0.23, CONSTANT("HATCH_ELV_LVL1"), false, 3.7));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, -180, 170, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 5));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_HOLD_DISTANCE, -250, 254, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 1.2));
+	//m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, -200, 270, 0.5, CONSTANT("HATCH_ELV_LVL1"), false, 5));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_VISION_ALIGN, 0, 270, -0, CONSTANT("HATCH_ELV_LVL1"), false, .2));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_VISION_ALIGN, 0, 270, -0.2, CONSTANT("HATCH_ELV_LVL1"), false, 2));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_INTAKE_EXHAUST, 0, 270, 0, CONSTANT("HATCH_ELV_LVL1"), false, 0.34));
+	m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, -20, 270, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 5));
+	//m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, 70, 0, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 5)); 
+    m_Iterator = m_Modes.begin();
+      
+    m_Modes["Two Hatch Cargo and Rocket - Right"];
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_RIGHT_ENCODER, 0, 0, 0, 5, true, 0.01));
+    //m_Modes["Two Hatch Cargo Ship - Right"].push_back(RobotCommand(CMD_PRESTART, 0, 0, 0, 0, true, 2)); 
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_ARM_DOWN, 0, 0, 0, 0, true, 0.25));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, 45, 0, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 1.5));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_VISION_ALIGN, 0, 0, -0.2, CONSTANT("HATCH_ELV_LVL1"), false, 7));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_INTAKE_EXHAUST, 0, 0, 0, CONSTANT("HATCH_ELV_LVL1"), false, 0.34));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, -18, 0, 0.7, CONSTANT("HATCH_ELV_LVL1"), false, 1.5));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_TURN, -18, 95, 0.7, CONSTANT("HATCH_ELV_LVL1"), true, 0.7));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, 69, 100, 0.64, CONSTANT("HATCH_ELV_LVL1"), false, 3));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, 80, 155, 0.4, CONSTANT("HATCH_ELV_LVL1"), false, 1.5));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_VISION_HATCH_INTAKE, 0, 180, -0.23, CONSTANT("HATCH_ELV_LVL1"), false, 3.7));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_DRIVE_DISTANCE, -140, 165, 0.8, CONSTANT("HATCH_ELV_LVL1"), false, 3));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_HOLD_DISTANCE, -270, 200, 0.35, CONSTANT("HATCH_ELV_LVL1"), false, 2));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_TURN, 0, 133, 0, CONSTANT("HATCH_ELV_LVL1"), false, 0.7));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_VISION_ALIGN, 0, 45, -0.2, CONSTANT("HATCH_ELV_LVL1"), false, 7));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_INTAKE_EXHAUST, 0, 45, 0, CONSTANT("HATCH_ELV_LVL1"), false, 0.5));
+    m_Modes["Two Hatch Cargo and Rocket - Right"].push_back(RobotCommand(CMD_HOLD_DISTANCE, -24, 133, 0.35, CONSTANT("HATCH_ELV_LVL1"), false, 2));
+    m_Iterator = m_Modes.begin();
+
 }
 
 std::deque<RobotCommand> AutoModes::GetCommandList()
